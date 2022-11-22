fbaa85130b199c3e0a12228a01ae7b511a0cb7f8
Hekaline <106314668+Hekaline@users.noreply.github.com>
2022-11-22 19:49:49 +0900

    Create App.js

---
+++ client/App.js
@@ -0,0 +1,11 @@
+/*import Login from "./Login";
+import Join1 from "./Join_G";
+import Registeration from "./Registeration";
+import Association from "./Association"; <-- Just ignore 'em */
+import Req from "./Req";
+
+const App = () => {
+  return <Req/>;
+}
+
+export default App;
