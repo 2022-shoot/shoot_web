015362f37ff6ec4010ec23917dc7f67d58fddb2f
Hekaline <106314668+Hekaline@users.noreply.github.com>
2022-11-22 19:25:01 +0900

    axios 요청 보내기 & 받은 값 출력하기
    e

---
+++ App.js
@@ -0,0 +1,11 @@
+import Login from "./Login";
+import Join1 from "./Join_G";
+import Registeration from "./Registeration";
+import Association from "./Association";
+import Req from "./Req";
+
+const App = () => {
+  return <Req/>;
+}
+
+export default App;
